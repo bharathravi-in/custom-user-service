53a8181f77ec333658c0703f581701d59d9e2c6c
devendra <devendra@devendras-MacBook-Pro.local>
2023-06-19 15:41:36 +0530

    Fix: Fetching xForm api bad request 400 error

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,35 @@
 FROM node:16 AS builder
 
 # Create app directory
-WORKDIR /app
+#WORKDIR /app
 
 # Install wget and gconf-service
 RUN apt-get update && apt-get install -y wget gconf-service
 
 # Install Chrome
-RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-RUN dpkg -i google-chrome-stable_current_amd64.deb
+# Install necessary dependencies 
+#RUN apt-get update &&    apt-get install -y wget gnupg ca-certificates &&     rm -rf /var/lib/apt/lists/*
+#RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - &&      echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list &&      apt-get update &&     apt-get install -y google-chrome-stable &&     rm -rf /var/lib/apt/lists/*
+
+#RUN apt-get install -y wget
+#RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+#RUN echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+#RUN apt-get update 
+#RUN apt-get -y install google-chrome-stable
+#RUN google-chrome --version
+#ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+#RUN echo "$PUPPETEER_EXECUTABLE_PATH"
+# Set Chrome as the Puppeteer executable path 
+#RUN echo 'export PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable' >> /etc/profile
+#ENV PATH PUPPETEER_EXECUTABLE_PATH:$PATH
+#RUN source ~/.bashrc
+#RUN cat ~/.bashrc
+#RUN  echo $PUPPETEER_EXECUTABLE_PATH
+#RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#RUN apt install -y ./google-chrome-stable_current_amd64.deb || apt-get --fix-broken install -y
+
+# Create app directory
+WORKDIR /app
 
 # copy dependency files
 COPY package.json ./
@@ -21,7 +42,8 @@ COPY .puppeteerrc.cjs ./
 COPY puppeteer-cache ./puppeteer-cache/
 
 # Install app dependencies
-RUN yarn install
+#RUN yarn cache clean
+RUN yarn install --ignore-scripts
 # Required if not done in postinstall
 # RUN npx prisma generate
 
@@ -41,5 +63,16 @@ COPY --from=builder /app/pdf ./pdf
 COPY --from=builder /app/.puppeteerrc.cjs ./.puppeteerrc.cjs
 COPY --from=builder /app/puppeteer-cache ./puppeteer-cache
 
+RUN apt-get install -y wget
+RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+RUN apt-get update
+RUN apt-get -y install google-chrome-stable
+RUN google-chrome --version
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+RUN echo "$PUPPETEER_EXECUTABLE_PATH"
+RUN google-chrome --version
+
 EXPOSE 3003
 CMD [ "npm", "run", "start:prod" ]
+~                                          
--- src/services/pdf.generator.service.ts
+++ src/services/pdf.generator.service.ts
@@ -2,6 +2,7 @@ import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
 import { IPDFResponse } from "../interfaces/common.interface";
 import puppeteer, { PaperFormat } from "puppeteer";
 import * as fs from "fs";
+const { URLSearchParams } = require('url');
 
 @Injectable()
 export class PDFService {
@@ -9,9 +10,54 @@ export class PDFService {
 
   async getPdf(url: string): Promise<IPDFResponse> {
     try {
-      const browser = await puppeteer.launch({ headless: "new" });
+      let xformUrl; 
+      const browser = await puppeteer.launch({ headless: "new", devtools: true, args: [
+            "--no-sandbox",
+            "--disable-gpu",
+        ] });
       const page = await browser.newPage();
-      await page.goto(url, { waitUntil: "networkidle2" });
+      await page.setRequestInterception(true); 
+      page.on('console', (message) => {   console.log(`Console message: ${message.text()}`); });
+      page.on('request', async (request) => {  
+        
+        // Check if the request is for the desired endpoint
+        if (request.url() === 'https://enketo.upsmfac.org/enketo/transform/xform') {
+          // Create the x-www-form-urlencoded body with the stored xformUrl
+          console.log(`Request URL: ${request.url()}`); 
+          const body = new URLSearchParams();
+          body.append('xformUrl', xformUrl);
+          console.log("-----request body----", request.postData());
+          console.log("-----request headers----", request.headers());
+          // Modify the request to include the modified body
+          console.log("#######Modified body and header####");
+          console.log("-----request body----", body.toString());
+          console.log("-----request headers----", request.headers());
+          await request.continue({
+            postData: body.toString(),
+            headers: {
+              ...request.headers(),
+            }
+          });
+        } else if (request.url().includes('https://enketo.upsmfac.org/enketo/preview')) {
+          // Extract xformUrl from the initial request URL
+          console.log(`Request URL: ${request.url()}`); 
+          const requestUrl = new URL(request.url());
+          xformUrl = requestUrl.searchParams.get('xform');
+          console.log("-----xformUrl---", xformUrl);
+          // Continue the request without modifying it
+          await request.continue();
+        } else {
+          // Continue the request without modifying it
+          await request.continue();
+        }
+      });      
+      page.on('response', (response) => {   
+        console.log(`Response URL: ${response.url()}`);   
+        console.log(`Response status: ${response.status()}`); 
+      });
+      // await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+      await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
+      await page.screenshot({ fullPage: true, path: 'screenshot.png' })
 
       // Wait for any dynamic content to load if necessary
       // e.g., page.waitForSelector('.some-element');
@@ -20,7 +66,9 @@ export class PDFService {
         format: "A4" as PaperFormat,
       };
       await page.pdf(pdfOptions);
+      // const source1 = await page.content();
 
+      // console.log("page content", source1);
       await browser.close();
       console.log(`PDF generated successfully`);
 
